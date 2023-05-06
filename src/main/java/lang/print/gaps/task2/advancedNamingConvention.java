8fe9d54134efc9e3f9db1522b66f019c3fa52d56
pomidorNagolove <harrier136@icloud.com>
2023-05-07 02:34:08 +0300

    upd all

--- src/main/java/lang/print/gaps/task2/advancedNamingConvention.java
+++ src/main/java/lang/print/gaps/task2/advancedNamingConvention.java
@@ -3,10 +3,10 @@ package lang.print.gaps.task2;
 public class advancedNamingConvention {
     private static final int adult_age = 18;
 
-    private int AGE;
+    private int age;
     private int phoneNumber;
 
-    void CAllToFriend() {
+    void callToFriend() {
         //some code
     }
 
